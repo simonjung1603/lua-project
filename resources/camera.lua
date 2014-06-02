04d26d4b13d5f2d1ede09426beabc244514af114
Sinalcoco <simonjung1603@gmail.com>
2014-06-01 22:55:33 -0600

    Implemented the camera module

---
+++ resources/camera.lua
@@ -0,0 +1,46 @@
+camera = {}
+camera.x = 0
+camera.y = 0
+camera.scaleX = 1
+camera.scaleY = 1
+camera.rotation = 0
+
+function camera:set()
+  love.graphics.push()
+  love.graphics.rotate(-self.rotation)
+  love.graphics.scale(1 / self.scaleX, 1 / self.scaleY)
+  love.graphics.translate(-self.x, -self.y)
+end
+
+function camera:unset()
+  love.graphics.pop()
+end
+
+function camera:move()
+  self.x = 0
+  if self.y > p.y - 400 then
+    self.y = p.y - 400
+  else
+    self.y = self.y
+  end
+end
+
+function camera:rotate(dr)
+  self.rotation = self.rotation + dr
+end
+
+function camera:scale(sx, sy)
+  sx = sx or 1
+  self.scaleX = self.scaleX * sx
+  self.scaleY = self.scaleY * (sy or sx)
+end
+
+function camera:setPosition(x, y)
+  self.x = x or self.x
+  self.y = y or self.y
+end
+
+function camera:setScale(sx, sy)
+  self.scaleX = sx or self.scaleX
+  self.scaleY = sy or self.scaleY
+end
