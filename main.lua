475a53275dd21e5ad4873de8f5cbc2717d301e36
Sinalcoco <simonjung1603@gmail.com>
2014-06-01 22:55:01 -0600

    switched out the main files
    I put it the main file I was working with this whole time and added
    require "resources/Map" to require you file

--- main.lua
+++ main.lua
@@ -1,3 +1,32 @@
+require "resources/Map"
+require "resources/player"
+require "resources/platforms"
+require "resources/camera"
+w = 150
+h = 20
+
 function love.load()
-  require "resource/map.lua"
-  end
+	--load
+	ground_fill(0, love.graphics.getHeight() - h, love.graphics.getWidth(), h)
+	ground_fill(500, 500, w, h)
+	ground_fill(300, 420, w, h)
+	ground_fill(100, 340, w, h)
+	ground_fill(300, 260, w, h)
+	ground_fill(500, 180, w, h)
+	ground_fill(300, 100, w, h)
+end
+
+function love.update(dt)
+	--update
+	PLAYER_UPDATE (dt)
+	camera:move ()
+end
+
+function love.draw()
+	--draw
+	camera:set ()
+	PLAYER_DRAW ()
+	ground_draw()
+	love.graphics.print("ground:"..p.ground,300,500)
+	camera:unset ()
+end
