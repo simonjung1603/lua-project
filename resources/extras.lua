38935fa7bfcd410aa3bf3a2bf551fb069b1e82d3
Sinalcoco <simonjung1603@gmail.com>
2014-06-04 01:53:28 -0600

    Added file for special extras
    I thought to include some specials like springs / trampolines that
    launch you into air and later on also to implement power ups so I
    created a file for that and even wrote a little code for springs
    already. Works great by the way

---
+++ resources/extras.lua
@@ -0,0 +1,23 @@
+s = {}
+s.x = 0
+s.y = 0
+s.w = 0
+s.h = 0
+visibility = false
+--adding a spring to the game
+function setSpring (x, y, w, h)
+	love.graphics.setColor(255,0,0)
+	s.x = x
+	s.y = y
+	s.w = w
+	s.h = h
+	for i,v in ipairs (g) do
+		love.graphics.rectangle("fill", s.x, s.y, s.w, s.h)
+	end
+	love.graphics.setColor(255,255,255)
+	if p.y + p.h < s.y + p.h and p.y + p.h > s.y 
+		and p.x + p.w / 2 > s.x and p.x + p.w / 2 < s.x + s.w
+		then
+		p.vy = 500
+	end
+end
