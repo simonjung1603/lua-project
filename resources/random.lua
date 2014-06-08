5a68b5df17a386f2b9dd827f054eec2e6a58fd3e
Sinalcoco <simonjung1603@gmail.com>
2014-06-08 15:34:01 -0600

    added coins

--- resources/random.lua
+++ resources/random.lua
@@ -7,29 +7,39 @@ function add_platform ()
 		n.h = v.h
 		n.x = math.random (0, love.graphics.getWidth() - n.w)
 		n.ex = math.random (0, 500)
-	end
-	m.x = math.random (0, love.graphics.getWidth() - n.w)
-	if m.x > 0 and m.x + n.w < love.graphics.getWidth() then
-		while m.x < n.x + n.w and m.x + n.w > n.x do
-			m.x = m.x + n.w + 100
-		end 
-	else
-		m.x = math.random (0, love.graphics.getWidth() - n.w)
-	end
-	m.y = n.y
+		if math.random (1,2) == 1 then
+			n.coin = true
+			m.coin = false
+		else
+			n.coin = false
+			m.coin = true
+		end
+		--print (i)
 
-	if n.ex <= 25 and n.ex > 0 then -- if it's a spring make it something different
-		m.ex = math.random (0, math.random (26, 500))
-	--elseif  --add different powerups 
-	else
-		m.ex = math.random(0, 500)
-	end 
+		m.x = math.random (0, love.graphics.getWidth() - n.w)
+		if m.x > 0 and m.x + n.w < love.graphics.getWidth() then
+			while m.x < n.x + n.w and m.x + n.w > n.x do
+				m.x = m.x + n.w + 100
+			end 
+		else
+			m.x = math.random (0, love.graphics.getWidth() - n.w)
+		end
+		m.y = n.y
+	
+		if n.ex <= 25 and n.ex > 0 then -- if it's a spring make it something different
+			m.ex = math.random (0, math.random (26, 500))
+		--elseif  --add different powerups 
+		else
+			m.ex = math.random(0, 500)
+		end 
+
+	end
 end
 
 function random_map ()
 --	for i = 1, 100, 1 do
 	add_platform ()
-	ground_fill (n.x, n.y, n.w, n.h, n.ex)
-	ground_fill (m.x, m.y, n.w, n.h, m.ex)
+	ground_fill (n.x, n.y, n.w, n.h, n.ex, n.coin)
+	ground_fill (m.x, m.y, n.w, n.h, m.ex, m.coin)
 --end
 end
