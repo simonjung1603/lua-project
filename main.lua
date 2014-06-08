3afe2075380a428713a1c6c4589365f0dd53d8b4
Sinalcoco <simonjung1603@gmail.com>
2014-06-08 15:35:22 -0600

    changed platform pool to 100 platforms

--- main.lua
+++ main.lua
@@ -14,8 +14,8 @@ function love.load()
 	math.randomseed (os.time())
 	love.graphics.setBackgroundColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
 	ground_fill(0, love.graphics.getHeight() - h, love.graphics.getWidth(), h, 0)
-	ground_fill(math.random(0, love.graphics.getWidth() - w), love.graphics.getHeight() - 100, w, h, 0)
-	for i = 1, 20, 1 do
+	ground_fill(math.random(0, love.graphics.getWidth() - w), love.graphics.getHeight() - 100, w, h, 0, true)
+	for i = 1, 50, 1 do
 		random_map ()
 	end
 	--ground_fill(300, 420, w, h, "spring")
@@ -39,8 +39,8 @@ function love.draw()
 	--draw
 	camera:set ()
 	PLAYER_DRAW ()
-	EXTRAS_DRAW ()
 	ground_draw()
+	EXTRAS_DRAW ()
 	--love.graphics.print("ground:"..ran,300,camera.y)
 	camera:unset ()
 end
