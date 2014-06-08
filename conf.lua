d3563a68c04b8efe2fdb79d9b3bc721c0fe557d9
Sinalcoco <simonjung1603@gmail.com>
2014-06-07 18:10:09 -0600

    Color change function

--- conf.lua
+++ conf.lua
@@ -1,6 +1,12 @@
 function love.conf(t)
-	t.window.title = "LuaTest1"
-    t.window.width = 800
+	t.window.title = "Frozen Element Studios - 1"
+    t.window.width = 1366
     t.window.height = 600
     t.console = true
+end
+
+function change_background ()
+	if love.keyboard.isDown("c") then
+		love.graphics.setBackgroundColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+	end
 end
