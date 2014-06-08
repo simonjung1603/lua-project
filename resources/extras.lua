9b813f98104d60d9cab2b0110fe19370610fd540
Sinalcoco <simonjung1603@gmail.com>
2014-06-07 18:09:44 -0600

    added boost to water

--- resources/extras.lua
+++ resources/extras.lua
@@ -19,7 +19,8 @@ function setSpring (x, y, w, h)
 	if p.y + p.h < s.y + s.h and p.y + p.h > s.y 
 		and p.x + p.w / 2 > s.x and p.x + p.w / 2 < s.x + s.w
 		then
-		p.vy = 500
+		p.vy = 500 + p.inc*3.5
+		
 	end
 end
 -- End of spring 
@@ -31,17 +32,13 @@ function water_move (dt)
 -- water_jump
 	if pause == "false" then 
 		if p.y - wa.y < - 200 
-			then wa.v = 200
+			then wa.v = 200 + p.inc
 			else
-				wa.v = 50
+				wa.v = 75
 		end
 	end
 -- water_rise 
 	wa.y = wa.y - wa.v * dt
--- water_collide
-	if p.y + p.h / 4 > wa.y and p.y + p.h / 4 < wa.y + wa.h 
-		then love.event.quit()
-	end
 end
 
 function water_draw ()
@@ -53,13 +50,16 @@ end
 
 -- Pause Button
 pause = "false"
+dead = "false"
 function pause_game ()
 	function love.keypressed(key) 
 		if key == "p" then
-			if pause == "false"  
-				then pause = "true"
-			else 
-				pause = "false"
+			if dead == "false" then
+				if pause == "false"  
+					then pause = "true"
+				else 
+					pause = "false"
+				end
 			end
 		end
 	end
@@ -71,7 +71,18 @@ function pause_game ()
 end
 -- End Of Pause Button
 
-
+-- ruler
+r = {y = love.graphics.getHeight()}
+function ruler ()
+	if p.y < r.y then
+		r.y = p.y
+	else
+		r.y = r.y
+	end
+	number = math.floor((r.y * -1) + love.graphics.getHeight() - plat:getHeight () - p.h + 0.5)
+	love.graphics.print("ruler:"..math.floor(number / 10 + 0.5),300,camera.y)
+end
+-- end of ruler
 
 
 --PARENT FUNCTIONS
@@ -79,8 +90,10 @@ function EXTRAS_UPDATE (dt)
 	pause_game ()
 	water_move (dt)
 
+
 end
 
 function EXTRAS_DRAW ()
 	water_draw ()
+	ruler ()
 end
